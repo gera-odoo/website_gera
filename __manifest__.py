8a2c7ddc517f73a26cfc755e9dde0fd75b7325e3
gera-odoo <gera@odoo.com>
2025-01-20 17:21:59 -0600

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+   'name': 'Airproof Theme',
+   'description': '...',
+   'category': 'Website/Theme',
+   'version': '1.0.0',
+   'author': '...',
+   'license': '',
+   'depends': ['website'],
+   'data': [
+      # ...
+   ],
+   'assets': {
+   'web._assets_primary_variables': [
+      ('prepend', 'website_gera/static/src/scss/primary_variables.scss'),
+   ],
+},
+}
